dc9b86325671f68b2bbacdfbe3f020f2f39e36ec
danyel oliveira <oliveiradanyel901@gmail.com>
2019-08-04 23:17:43 -0600

    stert firebase

---
+++ assets/JavaScript/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCFe022nkmCELTnvdLMr9g9IdXRg8KqJxQ",
+    authDomain: "train-scheduler-419ea.firebaseapp.com",
+    databaseURL: "https://train-scheduler-419ea.firebaseio.com",
+    projectId: "train-scheduler-419ea",
+    storageBucket: "train-scheduler-419ea.appspot.com",
+    messagingSenderId: "146986506562",
+    appId: "1:146986506562:web:777ba010de222879"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
+});
