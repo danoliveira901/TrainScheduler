815f89078c1ff8def43f1d3c8078d1d6fcc1aa67
danyel oliveira <oliveiradanyel901@gmail.com>
2019-08-20 23:32:31 -0600

    js finished and working with firestore

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,6 +1,5 @@
-$(document).ready(function(){
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
+$(document).ready(function() {
+  var config = {
     apiKey: "AIzaSyCFe022nkmCELTnvdLMr9g9IdXRg8KqJxQ",
     authDomain: "train-scheduler-419ea.firebaseapp.com",
     databaseURL: "https://train-scheduler-419ea.firebaseio.com",
@@ -9,8 +8,61 @@ $(document).ready(function(){
     messagingSenderId: "146986506562",
     appId: "1:146986506562:web:777ba010de222879"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(config);
 
-  
+  var database = firebase.database();
+
+  // Variables for the onClick event
+  var name;
+  var destination;
+  var firstTrain;
+  var frequency = 0;
+
+  $("#add-train").on("click", function() {
+      event.preventDefault();
+      // Storing and retreiving new train data
+      name = $("#TrainName").val().trim();
+      destination = $("#Destination").val().trim();
+      firstTrain = $("#TrainTime").val().trim();
+      frequency = $("#Frequency").val().trim();
+    // temporary object for holding new train data 
+    var tempTrain = {
+    name:name,
+    destination:destination,
+    firstTrain:firstTrain,
+    frequency:frequency
+    };
+
+      // Pushing to database
+      database.ref().push(tempTrain)
+        console.log(tempTrain.name)
+      
+      
+    
+  });
+
+  database.ref().on("child_added", function(childSnapshot) {
+     
+      var minAway;
+      // Chang year so first train comes before now
+      var firstTrainNew = moment(childSnapshot.val().firstTrain, "hh:mm").subtract(1, "years");
+      // Difference between the current and firstTrain
+      var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+      var remainder = diffTime % childSnapshot.val().frequency;
+      // Minutes until next train
+      var minAway = childSnapshot.val().frequency - remainder;
+      // Next train time
+      var nextTrain = moment().add(minAway, "minutes");
+      nextTrain = moment(nextTrain).format("hh:mm");
+
+      $("#Current-Train-table").append("<tr><td>" + childSnapshot.val().name +
+              "</td><td>" + childSnapshot.val().destination +
+              "</td><td>" + childSnapshot.val().frequency +
+              "</td><td>" + nextTrain + 
+              "</td><td>" + minAway + "</td></tr>");
+
+          // Handle the errors
+      }, function(errorObject) {
+          console.log("Errors handled: " + errorObject.code);
+  });
 });
